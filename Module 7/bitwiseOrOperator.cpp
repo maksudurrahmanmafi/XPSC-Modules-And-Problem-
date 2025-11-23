76b978a668df827abf54df6d958aafa5e59b4588
Mafi2482 <maksudurrahmanmafi0@gmail.com>
2025-11-23 14:13:38 +0600

    Bitwise AND  and Bitwise OR operator

---
+++ Module 7/bitwiseOrOperator.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a,b;//duitar akta bit o jodi 1 hoi tahole true return korbe
+    cin>>a>>b;
+    int ans = a|b;
+    cout<<ans;
+
+    return 0;
+}
