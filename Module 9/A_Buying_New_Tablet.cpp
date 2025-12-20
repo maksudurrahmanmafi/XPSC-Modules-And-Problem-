b129506b5b23b0fd48f8898e71e957c7230bf6ff
Mafi2482 <maksudurrahmanmafi0@gmail.com>
2025-12-20 19:24:19 +0600

    Buying Tablate

---
+++ Module 9/A_Buying_New_Tablet.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int n, b;
+        cin >> n >> b;
+        int ans = INT_MIN;
+
+        while (n--)
+        {
+            int x, y, z;
+            cin >> x >> y >> z;
+            if (z <= b)
+            {
+                ans = max(ans, x * y);
+            }
+        }
+        if(ans==INT_MIN)cout<<"no tablet\n";
+        else cout<<ans<<'\n';
+    }
+    return 0;
+}
