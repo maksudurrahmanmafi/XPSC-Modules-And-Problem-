745364aade834e42c91994d22587856138071bde
jui1960 <juifull196044@gmail.com>
2026-01-07 20:26:25 +0600

    circle

---
+++ Codeforces/circle.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    double r;
+    cin>>r;
+    double n = 3.14159;
+    cout<<fixed<<setprecision(4)<<"A="<<n*(r*r)<<'\n';
+    return 0;
+}
---
+++ Codeforces/hudai.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int x;
+    cin>>x;
+    int y;
+    cin>>y;
+    cout<<"X = "<<x+y<<'\n';
+    return 0;
+}
